e115571cc3b0ae72f0bbd657208672a7a5ca6d23
Dmitry Torgashin <torgashin@skbkontur.ru>
2026-01-08 21:18:54 +0500

    stashed 3

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,3 +1,5 @@
 export default {
-  '**/*.{js,ts,tsx}': 'echo 123',
+  '**/*.{js,ts,tsx,json,md,yml}': 'prettier --write',
+  '**/*.{js,ts,tsx}': 'eslint --max-warnings=0',
+  '**/*.ts?(x)': () => 'tsc -p tsconfig.json --noEmit',
 };
